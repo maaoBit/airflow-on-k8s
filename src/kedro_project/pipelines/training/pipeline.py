87727ba079aa52ceac802366f5c4672398f823d4
maao <maao420691301@gmail.com>
2025-10-20 11:21:06 +0800

    reflector
    Signed-off-by: maao <maao420691301@gmail.com>

--- src/kedro_project/pipelines/training/pipeline.py
+++
@@ -1,9 +0,0 @@
-from kedro.pipeline import Pipeline, node
-from .nodes import load_data, split_data, train_model
-
-def create_pipeline(**kwargs):
-    return Pipeline([
-        node(load_data, None, "raw_data"),
-        node(split_data, "raw_data", ["X_train", "X_test", "y_train", "y_test"]),
-        node(train_model, ["X_train", "X_test", "y_train", "y_test"], "model"),
-    ])
